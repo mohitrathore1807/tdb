19ea23983901f3277a972759813ec46d89a6ec0a
HP <mohitrathore1807@gmail.com>
2021-03-27 17:44:13 +0530

    data free

--- assets/tdbapp/static/resp.js
+++ assets/tdbapp/static/resp.js
@@ -7,3 +7,10 @@ burger.addEventListener('click', ()=>{
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
 });
+
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
