174ef1b99dec2527da880017fffa82f06ca3adb5
ruchirtoshniwal <ruchirtoshniwal450@gmail.com>
2021-06-21 13:04:55 +0530

    test1

--- tdbai/settings.py
+++ tdbai/settings.py
@@ -120,14 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS =[BASE_DIR,'static',]
+
 STATIC_ROOT= os.path.join(BASE_DIR,'assets')
+
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATETIME_FORMAT = '%d-%m-%Y %H:%M:%S'
+
 
 
 
