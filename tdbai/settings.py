3453959bb4d6ac7822c588289c3fd373b642626d
ruchirtoshniwal <ruchirtoshniwal450@gmail.com>
2021-07-13 05:41:25 +0530

    Server error solved

--- tdbai/settings.py
+++ tdbai/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tdbai.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.Djangotemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
